d97a74dfd3cdebb5a868ce376f078960e536da99
Shea Kelly <s.kelly@nib.com.au>
2017-06-26 23:04:28 +0000

    able to publish to topic

--- aws-nodejs/handler.js
+++ aws-nodejs/handler.js
@@ -4,11 +4,11 @@ const emailValidator = require('email-validator');
 const AWS = require('aws-sdk');
 
 const signup = (event, context, callback) => {
-  console.log(`event: ${event}`);
-  console.log(`context: ${context}`);
+  console.log(`event: ${JSON.stringify(event)}`);
+  console.log(`context: ${JSON.stringify(context)}`);
 
   const data = JSON.parse(event.body);
-  console.log(`data: ${data}`);
+  console.log(`data: ${JSON.stringify(data)}`);
 
   const validationError = validateSignup(data);
   if(validationError) {
@@ -17,16 +17,17 @@ const signup = (event, context, callback) => {
   }
 
   const sns = new AWS.SNS();
-  const publishParams = {
-    TopicArn: 'arn:aws:sns:ap-southeast-2:997909391502:mailing-list-signup-received'
-    Message: data
+  const params = {
+    TopicArn: 'arn:aws:sns:ap-southeast-2:997909391502:mailing-list-signup-received',
+    Message: JSON.stringify(data)
   };
   sns.publish(params, (err, data) => {
     if(err) {
-      callback(null, createResponse(500, 'Unable to publish to message');
+      console.log(err);
+      callback(null, createResponse(500, 'Unable to publish to message'));
       return;
     }
-    callback(null, createResponse(200, 'Congratulation you have signed up to the newsletter!');
+    callback(null, createResponse(200, 'Congratulation you have signed up to the newsletter!'));
   });
 };
 
@@ -37,7 +38,7 @@ const validateSignup = data => {
   if(!data.lastName) {
     return 'Last name is invalid';
   }
-  if(emailValidator.validate(data.email)) {
+  if(!emailValidator.validate(data.email)) {
     return 'Email is invalid';
   }
   return null;
