9ba61caa5ff47b50cc9337d5b187b35368443ba4
Shea Kelly <s.kelly@nib.com.au>
2017-06-26 11:00:06 +0000

    starting build of aws example

--- aws-nodejs/handler.js
+++ aws-nodejs/handler.js
@@ -1,16 +1,63 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
+const emailValidator = require('email-validator');
+const AWS = require('aws-sdk');
+
+const signup = (event, context, callback) => {
+  console.log(`event: ${event}`);
+  console.log(`context: ${context}`);
+
+  const data = JSON.parse(event.body);
+  console.log(`data: ${data}`);
+
+  const validationError = validateSignup(data);
+  if(validationError) {
+    callback(null, createResponse(400, validationError));
+    return;
+  }
+
+  const sns = new AWS.SNS();
+  const publishParams = {
+    TopicArn: 'arn:aws:sns:ap-southeast-2:997909391502:mailing-list-signup-received'
+    Message: data
+  };
+  sns.publish(params, (err, data) => {
+    if(err) {
+      callback(null, createResponse(500, 'Unable to publish to message');
+      return;
+    }
+    callback(null, createResponse(200, 'Congratulation you have signed up to the newsletter!');
+  });
+};
+
+const validateSignup = data => {
+  if(!data.firstName) {
+    return 'First name is invalid';
+  }
+  if(!data.lastName) {
+    return 'Last name is invalid';
+  }
+  if(emailValidator.validate(data.email)) {
+    return 'Email is invalid';
+  }
+  return null;
+};
+
+const sendWelcomeEmail = (event, context, callback) => {
+  console.log(`event: ${event}`);
+  console.log(`context: ${context}`);
+
+  //connect to mandrill
+  //send welcome email
+};
+
+const createResponse = (statusCode, message) => {
+ return {
+    statusCode: statusCode,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: message,
     }),
   };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports = {signup, sendWelcomeEmail};
